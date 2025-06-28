4833b12eb99f33cc8ec0b34b2d51ac3020d7a648
presashanette <125580618+presashanette@users.noreply.github.com>
2025-06-28 19:09:17 +0800

    Update SubtractInstruction.cpp

--- MO1_2/MO1_2/SubtractInstruction.cpp
+++ MO1_2/MO1_2/SubtractInstruction.cpp
@@ -5,24 +5,20 @@
 #include <cstdint>
 #include <sstream>
 
-// Constructor definition
 SubtractInstruction::SubtractInstruction(const std::string& result, const std::string& lhs, const std::string& rhs, const std::string& logPrefix)
     : resultVar(result), arg1(lhs), arg2(rhs), logPrefix(logPrefix) {
 }
 
 void SubtractInstruction::execute(std::shared_ptr<Process> proc, int coreId) {
-    // Auto-declare missing variables
     if (proc->memory.find(arg1) == proc->memory.end()) proc->memory[arg1] = 0;
     if (proc->memory.find(arg2) == proc->memory.end()) proc->memory[arg2] = 0;
 
     uint16_t val1 = proc->memory[arg1];
     uint16_t val2 = proc->memory[arg2];
 
-    // Subtract (no negatives)
     uint16_t result = (val1 > val2) ? (val1 - val2) : 0;
     proc->memory[resultVar] = result;
 
-    // Create proper log entry with timestamp
     std::ostringstream logEntry;
     if (logPrefix != "") {
         logEntry << "[" << getCurrentTimestamp() << "] "
