3c1d121be05d1dfa1faef7fd18c1974bfd73651c
presashanette <125580618+presashanette@users.noreply.github.com>
2025-06-28 19:11:27 +0800

    Update main.cpp

--- MO1_2/MO1_2/main.cpp
+++ MO1_2/MO1_2/main.cpp
@@ -4,10 +4,6 @@
 
 int main() {
     Config& config = Config::getInstance();
-    /*if (!config.loadFromFile("config.txt")) {
-        std::cerr << "Failed to load config. Using defaults.\n";
-    }*/
-
 
     CLIManager cli;
     cli.run();
